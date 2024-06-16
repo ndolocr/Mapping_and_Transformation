49b45268a9efa098d66afcdf788cb26c1d145d98
ndolocr <crndolo@gmail.com>
2024-06-16 16:57:15 +0300

    Create HTML Page to upload mapping file

--- file_upload/urls.py
+++ file_upload/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from file_upload import views
 
 urlpatterns = [
-    
+    path('',views.upload_file, name='upload_file')
 ]
--- file_upload/views.py
+++ file_upload/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def upload_file(request):
+    if request.method == 'GET':
+        return render(request, 'file_upload/upload_file.html')
