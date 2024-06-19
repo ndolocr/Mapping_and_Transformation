1e3d5ffee49ae458439cc70f871a1f767dd2b3a9
Raphael Ndolo <raphaelndolo@Raphaels-MacBook-Pro.local>
2024-06-19 18:07:00 +0300

    review Code to capture csv values

--- file_upload/views.py
+++ file_upload/views.py
@@ -1,29 +1,38 @@
-import openpyxl
+import csv
+from io import TextIOWrapper
 from django.shortcuts import render
 
 # Create your views here.
 def upload_file(request):
-    if request.method == 'GET':
+    if request.method == 'POST':
+        myFile = request.FILES["myFile"]
+        if myFile.name.endswith('.csv'):
+            csv_file = TextIOWrapper(myFile.file, encoding='utf-8')            
+            csv_reader = csv.reader(csv_file)
+            next(csv_reader)
+            for row in csv_reader:
+                firstname = row[0]
+                lastname = row[1]
+                birthdate = row[2]
+                mailingstreet = row[3]
+                mailingcity = row[4]
+                mailingstate = row[5]
+                mailingcountry = row[6]
+                mailingpostcode = row[7]
+                email = row[8]
+                mobilephonenumber = row[8]
+                homephonenumber = row[9]
+                employeetitle = row[10]
+                employername = row[11]
+                employerindustry = row[12]
+                employerstreet = row[13]
+                employercity = row[14]
+                employerstate = row[15]
+                employerpostcode = row[16]
+                employercountry = row[17]
+                employeremail = row[18]
+                employerphone = row[19]
+                employerfax = row[20]
         return render(request, 'file_upload/upload_file.html')
     else:
-        print("Uploading file...")
-        excel_file = request.FILES["myFile"]
-
-        if excel_file.name.endswith('.xlsx') or excel_file.name.endswith('.xls'):
-            with open('file/static/upload/' + excel_file.name, 'wb+') as destination:
-                for chunk in excel_file.chunks():
-                    destination.write(chunk)
-
-                wb = openpyxl.load_workbook(destination)
-                
-                worksheet = wb.active
-
-                worksheet_rows = []
-                for i in range(1, worksheet.max_row):
-                    row_data = []
-                    for col in worksheet.iter_cols(1, 11):                        
-                        row_data.append(col[i].value)
-                    if any(row_data):
-                        worksheet_rows.append(row_data)
-                            
-                context = { "worksheet_rows":worksheet_rows }
+        return render(request, 'file_upload/upload_file.html')
