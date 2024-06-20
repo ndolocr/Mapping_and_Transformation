b3a9a7816d3016046316a5f5ff4b74e38426c6b3
Raphael Ndolo <raphaelndolo@Raphaels-MacBook-Pro.local>
2024-06-20 18:11:30 +0300

    Creating a base HTML file with navigation bar

--- Mapping_and_Transformation/urls.py
+++ Mapping_and_Transformation/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_call.urls')),
     path('html/form/', include('html_form.urls')),
-    path('file/upload/', include('file_upload.urls')),    
+    path('', include('file_upload.urls')),    
 ]
