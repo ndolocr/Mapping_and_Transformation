3b8b70a3e89ec3b683abc79a5440dea5c6562d51
Raphael Ndolo <raphaelndolo@Raphaels-MacBook-Pro.local>
2024-06-20 17:16:24 +0300

    Create a html web page to capture source data on html page

--- html_form/views.py
+++ html_form/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from Mapping_and_Transformation.utilities import transformation_matrix
 
 # Create your views here.
 def create_turtle_script(request):
     if request.method == "GET":
-        pass
+        return render(request, 'html_form/index.html')
     else:
         pass
